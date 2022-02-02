7ee676154fd280379735e5b53db4e8c2b31e0f7c
Naim Jhuboo <naim.jhuboo@gmail.com>
2022-02-02 15:36:45 +0100

    first commit

---
+++ src/Entity/Game.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GameRepository::class)
+ */
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photoMap;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photoRapper;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $searchSound;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $victorySound;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $positionX;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $positionY;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $level;
+
+    /**
+     * @ORM\OneToMany(targetEntity=User::class, mappedBy="game")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhotoMap(): ?string
+    {
+        return $this->photoMap;
+    }
+
+    public function setPhotoMap(string $photoMap): self
+    {
+        $this->photoMap = $photoMap;
+
+        return $this;
+    }
+
+    public function getPhotoRapper(): ?string
+    {
+        return $this->photoRapper;
+    }
+
+    public function setPhotoRapper(string $photoRapper): self
+    {
+        $this->photoRapper = $photoRapper;
+
+        return $this;
+    }
+
+    public function getSearchSound(): ?string
+    {
+        return $this->searchSound;
+    }
+
+    public function setSearchSound(string $searchSound): self
+    {
+        $this->searchSound = $searchSound;
+
+        return $this;
+    }
+
+    public function getVictorySound(): ?string
+    {
+        return $this->victorySound;
+    }
+
+    public function setVictorySound(string $victorySound): self
+    {
+        $this->victorySound = $victorySound;
+
+        return $this;
+    }
+
+    public function getPositionX(): ?int
+    {
+        return $this->positionX;
+    }
+
+    public function setPositionX(int $positionX): self
+    {
+        $this->positionX = $positionX;
+
+        return $this;
+    }
+
+    public function getPositionY(): ?int
+    {
+        return $this->positionY;
+    }
+
+    public function setPositionY(int $positionY): self
+    {
+        $this->positionY = $positionY;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getGame() === $this) {
+                $user->setGame(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Game::class, inversedBy="users")
+     */
+    private $game;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getGame(): ?Game
+    {
+        return $this->game;
+    }
+
+    public function setGame(?Game $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+}
